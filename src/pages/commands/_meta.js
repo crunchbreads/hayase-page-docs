8571c3accff801ae97f7d5d7b3b4e3e060a97ac2
dayrain <blackwaterbread@gmail.com>
2024-10-14 01:12:34 +0900

    docs: reflect service changes

--- src/pages/commands/_meta.js
+++ src/pages/commands/_meta.js
@@ -1,10 +1,10 @@
 export default {
   'help': '도움말',
   'initialize': '채널 설정 (자동)',
+  'set-channel-status': '채널 설정 (수동) (서버현황)',
   'server-register': '서버 등록',
   'server-remove': '서버 삭제',
   'premium': '프리미엄 기간 확인',
   'set-language': '언어 설정',
-  'set-channel-status': '채널 설정 (서버현황)',
-  'set-channel-players': '채널 설정 (플레이어)',
+  'set-channel-players': '채널 설정 (플레이어 카운트)',
 }
