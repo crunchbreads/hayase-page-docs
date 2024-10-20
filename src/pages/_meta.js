499c7bfbaf536aac1b6e5d83b6bd68f7b591566c
dayrain <blackwaterbread@gmail.com>
2024-10-20 20:04:39 +0900

    docs: add changelog

--- src/pages/_meta.js
+++ src/pages/_meta.js
@@ -5,6 +5,7 @@ export default {
   games: '지원 게임',
   languages: '지원 언어',
   troubleshooting: '문제 해결',
+  changelog: '업데이트',
   discord_link: {
     title: '홈페이지',
     href: 'https://hayasebot.app',
