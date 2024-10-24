4e1643242ab7401c05ddab63cfabb0258def915c
dayrain <blackwaterbread@gmail.com>
2024-10-24 12:08:58 +0900

    style: font antialiased

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="https://docs.hayasebot.app/assets/static/eyecatch.webp" />
       </Head>
-      <main className={pretendard.className}>
+      <main className={`${pretendard.className} antialiased`}>
         <div className="w-full px-8 py-1.5 flex justify-center bg-gradient-to-r from-rose-500 via-purple-500 to-pink-500">
           <span className='select-none text-gray-200 text-md md:text-md font-bold'>현재 알파 테스트 중으로 서비스 불안정이나 오작동의 가능성이 있습니다. 양해 부탁드립니다.</span>
         </div>
